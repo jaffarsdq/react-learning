6e65285ae5981f7d2a57f95b70b8c4881d94a23e
Jaffar sadhiq <jaffarsdq007@gmail.com>
2023-07-26 13:59:03 +0530

    added step by step format form

--- Form-React/form-validation/src/components/Form/Form.jsx
+++ Form-React/form-validation/src/components/Form/Form.jsx
@@ -1,6 +1,6 @@
 import './form.css'
 import Input from '../Input/Input';
-import { useContext, useRef } from 'react';
+import { useContext, useRef, useState } from 'react';
 import { FormContext } from '../providers/FormContext';
 
 import validateMail from '../../helpers/validateMail';
@@ -9,8 +9,11 @@ import validatePassword from '../../helpers/validatePassword';
 function Form() {
 
     const {formInput} = useContext(FormContext);
+    const [step, setStep] = useState(0);
     const emailRef = useRef(null);
     const passwordRef = useRef(null);
+    const usernameRef = useRef(null);
+    const addressRef = useRef(null);
 
     const handleFormSubmit = (e) => {
         e.preventDefault();
@@ -32,44 +35,132 @@ function Form() {
         }
     }
 
-    return (
-        <form className='form-wrapper' onSubmit={handleFormSubmit}noValidate>
-            <h1>Form Application</h1>
+    if(step == 0) {
+        return (
+            <form className='form-wrapper' onSubmit={handleFormSubmit}noValidate>
+                <h1>Form Application</h1>
+    
+                <div className="mail-wrapper wrapper">
+                    <label htmlFor="mail">Mail</label>
+                    <Input
+                     type='email' 
+                     name="mail" 
+                     id="mail" 
+                     placeholder='Mail'
+                     label="email"
+                     ref={emailRef}
+                     key={1}
+                     checkOnBlur={handleInvalidEmail}
+                    />
+                </div>
+    
+                <div className='password-wrapper wrapper'>
+                    <label htmlFor="password">Password</label>
+                    <Input
+                     type='password' 
+                     name="password" 
+                     id="password" 
+                     placeholder='Password'
+                     label = "password"
+                     ref={passwordRef}
+                     key={2}
+                     checkOnBlur={handleInvalidPassword}
+                    />
+                </div>
+                
+                <div className="buttons">
+                    <div className='submit-btn'>
+                        <button type='submit' >
+                            Submit
+                        </button>
+                    </div>
 
-            <div className="mail-wrapper wrapper">
-                <label htmlFor="mail">Mail</label>
-                <Input
-                 type='email' 
-                 name="mail" 
-                 id="mail" 
-                 placeholder='Mail'
-                 label="email"
-                 ref={emailRef}
-                 checkOnBlur={handleInvalidEmail}
-                />
-            </div>
-
-            <div className='password-wrapper wrapper'>
-                <label htmlFor="password">Password</label>
-                <Input
-                 type='password' 
-                 name="password" 
-                 id="password" 
-                 placeholder='Password'
-                 label = "password"
-                 ref={passwordRef}
-                 checkOnBlur={handleInvalidPassword}
-                />
-            </div>
+                    <div className='next-btn'>
+                        <button type='button' onClick={() => setStep(step+1)} >
+                            Next
+                        </button>
+                    </div>
+                </div>
             
-            <div className='submit-btn'>
-                <button type='submit' >
-                    Submit
-                </button>
-            </div>
-        
-        </form>
-    )
+            </form>
+        )
+    } else if(step == 1) {
+        return (
+            <form className='form-wrapper' onSubmit={handleFormSubmit}noValidate>
+                <h1>Form Application</h1>
+    
+                <div className="mail-wrapper wrapper">
+                    <label htmlFor="username">Username</label>
+                    <Input
+                     type='text' 
+                     name="username" 
+                     id="username" 
+                     placeholder='User name'
+                     label="username"
+                     ref={usernameRef}
+                     key={3}
+                     checkOnBlur={handleInvalidEmail}
+                    />
+                </div>
+                
+                <div className="buttons">
+                    <div className='previous-btn'>
+                        <button onClick={() => setStep(step-1)} >
+                            Previous
+                        </button>
+                    </div>
+
+
+                    <div className='submit-btn'>
+                        <button type='submit' >
+                            Submit
+                        </button>
+                    </div>
+                
+                    <div className='next-btn'>
+                        <button onClick={() => setStep(step+1)} >
+                            Next
+                        </button>
+                    </div>
+                </div>
+            </form>
+        )
+    } else if(step == 2) {
+        return (
+            <form className='form-wrapper' onSubmit={handleFormSubmit}noValidate>
+                <h1>Form Application</h1>
+    
+                <div className="mail-wrapper wrapper">
+                    <label htmlFor="address">Address</label>
+                    <Input
+                     type='address' 
+                     name="address" 
+                     id="address" 
+                     placeholder='Address'
+                     label="address"
+                     ref={addressRef}
+                     key={4}
+                     checkOnBlur={handleInvalidEmail}
+                    />
+                </div>
+
+                <div className="buttons">
+                    <div className='previous-btn'>
+                        <button onClick={() => setStep(step-1)}>
+                            Previous
+                        </button>
+                    </div>
+                    
+                    <div className='submit-btn'>
+                        <button type='submit' >
+                            Submit
+                        </button>
+                    </div>
+                </div>
+            
+            </form>
+        )
+    }
 }
 
 export default Form;
